348ab4edeb2f520f1fb7f1bf9a7846889b42ec70
Yurii Denkovych <yd2004@ukr.net>
2022-09-15 00:34:18 +0300

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,18 @@
 import ContactListItem from 'components/ContactListItem';
 import s from './ContactList.module.css';
 
-import { selectFilter, useGetContactsQuery } from 'redux/contactSlice';
-import { useSelector } from 'react-redux/es/exports';
+import {  useGetContactsQuery } from 'redux/contactSlice';
+//import { useSelector } from 'react-redux/es/exports';
 
 export default function ContactList() {
   const { data: contacts } = useGetContactsQuery();
-  const filter = useSelector(selectFilter);
-  console.log(filter)
-  const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
+  //const filter = useSelector(selectFilter);
+  console.log({ data: contacts })
+  //const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
   
     return (
     <ul className={s.contactList}>
-      { visibleContacts.map(({ name, phone, id }) => (
+      { contacts?.map(({ name, phone, id }) => (
               <ContactListItem name={name} phone={phone} key={id} id={id} />
             ))}
     </ul>
